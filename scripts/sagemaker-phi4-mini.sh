b929857f510e8a16a493e046506f183c839cddfb
jpospinalo <jpospinalo@unal.edu.co>
2025-12-04 17:58:04 -0500

    Update sagemaker-phi4-mini.sh

--- scripts/sagemaker-phi4-mini.sh
+++ scripts/sagemaker-phi4-mini.sh
@@ -2,11 +2,11 @@ docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
 
 sudo systemctl enable --now docker
 
-docker exec -it ollama ollama pull llama3.2:3b
+docker exec -it ollama ollama pull phi4-mini:3.8b
 
 
 # {
-#   "model": "llama3.2:3b",
+#   "model": "phi4-mini:3.8b",
 #   "prompt": "responde en 30 palabras. ¿qué modelo eres?",
 #   "stream": false,
 #   "keep_alive": -1,
